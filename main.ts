c9ceec0020362adbc8be058af7f04b6117e39967
Gxug <92217870+Gxug@users.noreply.github.com>
2021-10-09 21:21:29 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,32 @@
-
+let I = 0
+input.onButtonPressed(Button.A, function () {
+    I = randint(1, 3)
+    if (I == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (I == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
